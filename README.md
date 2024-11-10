ca119a61d07d36072869832099f11c08108dd06d
TungsticFish <yezhuoxian01@gmail.com>
2024-11-11 00:29:30 +0800

    1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# 吟游诗人音乐播放器
+此在线工具可帮助DND5版吟游诗人来弹奏随机小曲。
+暂时只做了逸闻学院、创造学院和精魂学院。
+
+# Bard_Music_Player 
+Music Player of DND Bard, for College of Lore, Creation and Spirits.
+
+by 钨鱼大磨蹭的业余时间
---
+++ js/script.js
@@ -0,0 +1,89 @@
+// JavaScript source code
+
+
+
+function readFiles() {
+
+    document.getElementById("music_list").innerHTML=''
+
+    var folderInput = document.getElementById('folderInput');
+    var fileList = folderInput.files;
+    
+
+    for (var i = 0; i < fileList.length; i++) {        
+        var file = fileList[i];
+        var file_list = document.createElement("button");//给元素添加属性
+        file_list.classList.add("row_1");
+        file_list.classList.add("item");
+        file_list.innerHTML=file.name;        
+        file_list.id=URL.createObjectURL(file)
+        file_list.onclick= function () {
+            //console.log(URL.createObjectURL(file))
+            audioElement.src = this['id'];    
+	        audioElement.play();
+        }
+
+        //监测是否是音频，只添加音频
+        if (file.type.match('audio.*')) {
+            document.getElementById("music_list").appendChild(file_list);
+        }
+        
+    }
+
+}
+
+
+
+
+//新播放
+let audioElement = new Audio('assets/EVA.mp3');
+let masterPlay = document.getElementById('masterPlay');
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+
+//绑定元素 const Bard_1 = document.getElementById('Bard_1');
+
+
+///添加函数响应 Bard_1.addEventListener('click', PlayMusic)
+
+
